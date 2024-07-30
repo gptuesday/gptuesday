c8e992b05bfa9a736f7a14db4209d6d959d317aa
grant <grant@consultabit.com>
2024-07-29 23:49:31 -0400

    attempting to adjust event card size

--- components/events/all-events.tsx
+++ components/events/all-events.tsx
@@ -7,7 +7,7 @@ export default function AllEvents() {
   return(
     <>
       {/* <div className="flex flex-wrap justify-center gap-14 m-auto w-full"> */}
-      <div className="flex flex-wrap justify-center gap-14 m-auto w-full">
+      <div className="flex flex-wrap justify-center gap-y-10 md:gap-14 md:gap-y-14 m-auto w-full">
         {events.map((event,index) => (
           <EventCard key={index} event={event} />
         ))}
